8ff8b87c4d054a2f73204f1d27bff49c1a30616b
Nakul Surve <nakul.rootuser@gmail.com>
2026-02-14 09:38:43 +0530

    Create outputs.tf

---
+++ environments/prod/outputs.tf
@@ -0,0 +1,26 @@
+# environments/prod/outputs.tf
+
+output "alb_url" {
+  description = "URL of the Application Load Balancer"
+  value       = "http://${module.webserver.alb_dns_name}"
+}
+
+output "vpc_id" {
+  description = "VPC ID"
+  value       = module.vpc.vpc_id
+}
+
+output "public_subnets" {
+  description = "Public subnet IDs"
+  value       = module.vpc.public_subnet_ids
+}
+
+output "private_subnets" {
+  description = "Private subnet IDs"
+  value       = module.vpc.private_subnet_ids
+}
+
+output "asg_name" {
+  description = "Auto Scaling Group name"
+  value       = module.webserver.asg_name
+}
