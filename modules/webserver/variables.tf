29f00b0abbac8ceaa2bb59fc5acf297936bb1756
Nakul Surve <nakul.rootuser@gmail.com>
2026-02-14 09:24:27 +0530

    Create variables.tf

---
+++ modules/webserver/variables.tf
@@ -0,0 +1,45 @@
+# modules/webserver/variables.tf
+
+variable "environment" {
+  description = "Environment name (dev/prod)"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID where resources will be created"
+  type        = string
+}
+
+variable "public_subnet_ids" {
+  description = "List of public subnet IDs for ALB"
+  type        = list(string)
+}
+
+variable "private_subnet_ids" {
+  description = "List of private subnet IDs for EC2 instances"
+  type        = list(string)
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "min_size" {
+  description = "Minimum number of instances in ASG"
+  type        = number
+  default     = 2
+}
+
+variable "max_size" {
+  description = "Maximum number of instances in ASG"
+  type        = number
+  default     = 4
+}
+
+variable "desired_capacity" {
+  description = "Desired number of instances in ASG"
+  type        = number
+  default     = 2
+}
